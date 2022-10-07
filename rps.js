c28733fd1fbe869bcec10cf2b0705f5c8e739713
Hvbert1 <henryjiafu@gmail.com>
2022-10-07 13:30:49 +1100

    create play round function that runs the game and returns results

--- rps.js
+++ rps.js
@@ -14,4 +14,33 @@ function playRound(playerSelection) {
         const computerOption = ["rock", "paper", "scissors"];
         return computerOption[Math.floor(Math.random() * computerOption.length)];
     }
+    
+    function playRound() {
+
+        const computerSelection = getComputerChoice();
+        console.log("computer selection:", computerSelection);
+        console.log(playerSelection)
+
+        if(playerSelection === computerSelection) {
+            return "Tie!"
+        }
+        if(playerSelection == "rock" && computerSelection == "paper") {
+            return "You Lose! Paper beats Rock"
+        }
+        if(playerSelection == "rock" && computerSelection == "scissors") {
+            return "You Win! Rock beats Scissors"
+        }
+        if(playerSelection == "paper" && computerSelection == "rock") {
+            return "You Win! Paper beats Rock"
+        }
+        if(playerSelection == "paper" && computerSelection == "scissors") {
+            return "You Lose! Scissors beats Paper"
+        }
+        if(playerSelection == "scissors" && computerSelection == "rock") {
+            return "You Lose! Rock beats Scissors"
+        }
+        if(playerSelection == "scissors" && computerSelection == "paper") {
+            return "You Win! Scissors beats Paper"
+        }
+    }
 }
