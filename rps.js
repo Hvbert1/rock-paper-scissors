e80d34e6cabb953e37a06a1536550e5af6b88809
Hvbert1 <henryjiafu@gmail.com>
2022-10-07 14:14:11 +1100

    fixed syntax

--- rps.js
+++ rps.js
@@ -6,49 +6,40 @@ function getComputerChoice() {
     return computerOption[Math.floor(Math.random() * computerOption.length)];
 }
 
-const computerSelection = getComputerChoice();
+function playRound() {
+    let choice = prompt("Rock, paper or scissors?")
+    const playerSelection = choice;
+    let result = "";
 
-function playRound(playerSelection) {
+    console.log("playerscore:" + playerScore);
+    console.log("compscore:" + computerScore);
 
-    function getComputerChoice() {
-        const computerOption = ["rock", "paper", "scissors"];
-        return computerOption[Math.floor(Math.random() * computerOption.length)];
-    }
+    const computerSelection = getComputerChoice();
+    console.log("computer selection:", computerSelection);
+    console.log(playerSelection)
 
-    function playRound() {
-        let choice = prompt("Rock, paper or scissors?")
-        const playerSelection = choice;
-        let result = "";
+    if ((playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "paper" && computerSelection == "rock") ||
+        (playerSelection == "scissors" && computerSelection == "paper")) {
+            playerScore++;
+            result = ("You win! " + playerSelection + " beats " + computerSelection)
 
-        console.log("playerscore:" + playerScore);
-        console.log("compscore:" + computerScore);
-
-        const computerSelection = getComputerChoice();
-        console.log("computer selection:", computerSelection);
-        console.log(playerSelection)
-
-        if ((playerSelection == "rock" && computerSelection == "scissors") ||
-            (playerSelection == "paper" && computerSelection == "rock") ||
-            (playerSelection == "scissors" && computerSelection == "paper")) {
-                playerScore++;
-                result = ("You win! " + playerSelection + " beats " + computerSelection)
-
-                if(playerScore == 5){
-                    result = ("You won the game!")
-                }
+            if(playerScore == 5){
+                result = ("You won the game!")
             }
-
-        else if(playerSelection === computerSelection) {
-            result =  "Tie!"
         }
+
+    else if(playerSelection === computerSelection) {
+        result =  "Tie!"
+    }
             
-        else {
-            computerScore++;
-            result = ("You lose! " + computerSelection + " beats " + playerSelection)
-            if (computerScore == 5) {
-                result = ("You lost the Game!")
-            }
+    else {
+        computerScore++;
+        result = ("You lose! " + computerSelection + " beats " + playerSelection)
+        if (computerScore == 5) {
+            result = ("You lost the Game!")
         }
-        return result;
     }
+    return result;
 }
+
